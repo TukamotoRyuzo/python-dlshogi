f899926bd98e38a4136747bca0f418896fc4eb08
TukamotoRyuzo <intelcore4616duow@icloud.com>
2018-09-28 00:13:44 +0900

    keras network definition

--- pydlshogi/network/policy.py
+++ pydlshogi/network/policy.py
@@ -1,41 +1,29 @@
-﻿from chainer import Chain
-import chainer.functions as F
-import chainer.links as L
+﻿import keras
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 
 from pydlshogi.common import *
 
 ch = 192
-class PolicyNetwork(Chain):
+class PolicyNetwork():
     def __init__(self):
-        super(PolicyNetwork, self).__init__()
-        with self.init_scope():
-            self.l1=L.Convolution2D(in_channels = 104, out_channels = ch, ksize = 3, pad = 1)
-            self.l2=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l3=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l4=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l5=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l6=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l7=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l8=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l9=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l10=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l11=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l12=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l13=L.Convolution2D(in_channels = ch, out_channels = MOVE_DIRECTION_LABEL_NUM, ksize = 1, nobias = True)
-            self.l13_bias=L.Bias(shape=(9*9*MOVE_DIRECTION_LABEL_NUM))
+        self.model = Sequential()
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same', input_shape = (9, 9, 104)))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same'))
+        self.model.add(Conv2D(filters = MOVE_DIRECTION_LABEL_NUM, kernel_size = (1, 1), activation = 'softmax', padding = 'same', use_bias = True))
+        self.model.compile(loss = keras.losses.categorical_crossentropy, optimizer = keras.optimizers.Adadelta(),metrics = ['accuracy'])
 
     def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        h2 = F.relu(self.l2(h1))
-        h3 = F.relu(self.l3(h2))
-        h4 = F.relu(self.l4(h3))
-        h5 = F.relu(self.l5(h4))
-        h6 = F.relu(self.l6(h5))
-        h7 = F.relu(self.l7(h6))
-        h8 = F.relu(self.l8(h7))
-        h9 = F.relu(self.l9(h8))
-        h10 = F.relu(self.l10(h9))
-        h11 = F.relu(self.l11(h10))
-        h12 = F.relu(self.l12(h11))
-        h13 = self.l13(h12)
-        return self.l13_bias(F.reshape(h13, (-1, 9*9*MOVE_DIRECTION_LABEL_NUM)))
+        return self.model.predict(x)
+
