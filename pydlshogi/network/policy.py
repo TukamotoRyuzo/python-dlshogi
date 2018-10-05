a22ce3a32b698c6cd84e7739e1500d265eddaa1a
r.tsukamoto <r.tsukamoto@pc-link.co.jp>
2018-10-05 20:53:06 +0900

    replace keras

--- pydlshogi/network/policy.py
+++ pydlshogi/network/policy.py
@@ -21,5 +21,6 @@ class PolicyNetwork(Sequential):
         self.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same', data_format = "channels_first"))
         self.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same', data_format = "channels_first"))
         self.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same', data_format = "channels_first"))
-        self.add(Conv2D(filters = MOVE_DIRECTION_LABEL_NUM, kernel_size = (1, 1), activation = 'softmax', padding = 'same', use_bias = True, data_format = "channels_first")))
+        self.add(Conv2D(filters = MOVE_DIRECTION_LABEL_NUM, kernel_size = (1, 1), activation = 'softmax', padding = 'same',
+         use_bias = True, data_format = "channels_first"))
         self.compile(loss = keras.losses.categorical_crossentropy, optimizer = keras.optimizers.SGD(), metrics = ['accuracy'])
