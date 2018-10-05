609e86c0f7a82725eee044fc061e4a6b52cec6eb
r.tsukamoto <r.tsukamoto@pc-link.co.jp>
2018-10-05 21:04:52 +0900

    replace keras

--- train_policy.py
+++ train_policy.py
@@ -109,6 +109,8 @@ for e in range(args.epoch):
     sum_loss_epoch = 0
     for i in range(0, len(positions_train_shuffled) - args.batchsize, args.batchsize):
         x, t = mini_batch(positions_train_shuffled, i, args.batchsize)
+        print(x.shape, t.shape)
+        
         hist = p_net.fit(x, t, batch_size=args.batchsize, epochs=1, verbose=0)
 
         itr += 1
