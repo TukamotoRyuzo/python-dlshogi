ec16e29e09fa8760ae9d5a2eb2e6df255262d536
r.tsukamoto <r.tsukamoto@pc-link.co.jp>
2018-10-10 18:55:19 +0900

    replace keras

--- pydlshogi/network/policy.py
+++ pydlshogi/network/policy.py
@@ -21,7 +21,7 @@ class PolicyNetwork(Sequential):
         self.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same', data_format = "channels_first"))
         self.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same', data_format = "channels_first"))
         self.add(Conv2D(filters = ch, kernel_size = (3, 3), activation = 'relu', padding = 'same', data_format = "channels_first"))
-        self.add(Conv2D(filters = MOVE_DIRECTION_LABEL_NUM, kernel_size = (1, 1), activation = 'softmax', padding = 'same',
+        self.add(Conv2D(filters = MOVE_DIRECTION_LABEL_NUM, kernel_size = (1, 1), activation = 'relu', padding = 'same',
          use_bias = True, data_format = "channels_first"))
         self.add(Reshape((MOVE_DIRECTION_LABEL_NUM * 9 * 9,)))
         self.add(Activation('softmax'))
