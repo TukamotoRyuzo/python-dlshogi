7644512a4b470702eeb71d690eb7bbb9dd98e8f1
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2018-10-15 19:10:46 +0900

    update policy player to use Keras instead of Chainer

--- pydlshogi/player/policy_player.py
+++ pydlshogi/player/policy_player.py
@@ -1,18 +1,15 @@
 ﻿import numpy as np
-import chainer
-from chainer import serializers
-from chainer import cuda, Variable
-import chainer.functions as F
 
-import shogi
+from keras.models import Model
 
-from pydlshogi.common import *
-from pydlshogi.features import *
-from pydlshogi.network.policy import *
-from pydlshogi.player.base_player import *
+import pydlshogi.features as fts
+from pydlshogi.network.policy import PolicyNetwork
+from pydlshogi.player.base_player import BasePlayer
+
 
 def greedy(logits):
-    return logits.index(max(logits))
+    return np.argmax(logits)
+
 
 def boltzmann(logits, temperature):
     logits /= temperature
@@ -21,10 +18,12 @@ def boltzmann(logits, temperature):
     probabilities /= probabilities.sum()
     return np.random.choice(len(logits), p=probabilities)
 
+
 class PolicyPlayer(BasePlayer):
-    def __init__(self):
+
+    def __init__(self, modelfile):
         super().__init__()
-        self.modelfile = r'H:\src\python-dlshogi\model\model_policy'
+        self.modelfile = modelfile
         self.model = None
 
     def usi(self):
@@ -39,8 +38,7 @@ class PolicyPlayer(BasePlayer):
     def isready(self):
         if self.model is None:
             self.model = PolicyNetwork()
-            self.model.to_gpu()
-        serializers.load_npz(self.modelfile, self.model)
+        self.model.load_weights(self.modelfile)
         print('readyok')
 
     def go(self):
@@ -48,31 +46,35 @@ class PolicyPlayer(BasePlayer):
             print('bestmove resign')
             return
 
-        features = make_input_features_from_board(self.board)
-        x = Variable(cuda.to_gpu(np.array([features], dtype=np.float32)))
-
-        with chainer.no_backprop_mode():
-            y = self.model(x)
-
-            logits = cuda.to_cpu(y.data)[0]
-            probabilities = cuda.to_cpu(F.softmax(y).data)[0]
+        features = fts.make_input_features_from_board(self.board)
+        # self.modelは確率を返すようになっている
+        # logitも欲しいのでsoftmax前の値を取り出すようにする
+        layer_name = 'reshape_1'
+        logit_model = Model(
+            inputs=self.model.input,
+            outputs=self.model.get_layer(layer_name).output)
+        logits = logit_model.predict(features)
+        # logitをsoftmaxしてprobabilityを計算
+        exp_logits = np.exp(logits)
+        probabilities = exp_logits / np.sum(exp_logits)
 
         # 全ての合法手について
         legal_moves = []
         legal_logits = []
         for move in self.board.legal_moves:
             # ラベルに変換
-            label = make_output_label(move, self.board.turn)
+            label = fts.make_output_label(move, self.board.turn)
             # 合法手とその指し手の確率(logits)を格納
             legal_moves.append(move)
             legal_logits.append(logits[label])
             # 確率を表示
-            print('info string {:5} : {:.5f}'.format(move.usi(), probabilities[label]))
-            
+            print('info string {:5} : {:.5f}'.format(move.usi(),
+                                                     probabilities[label]))
+
         # 確率が最大の手を選ぶ(グリーディー戦略)
         selected_index = greedy(legal_logits)
         # 確率に応じて手を選ぶ(ソフトマックス戦略)
-        #selected_index = boltzmann(np.array(legal_logits, dtype=np.float32), 0.5)
+        # selected_index = boltzmann(np.array(legal_logits, dtype=np.float32), 0.5)
         bestmove = legal_moves[selected_index]
 
         print('bestmove', bestmove.usi())
