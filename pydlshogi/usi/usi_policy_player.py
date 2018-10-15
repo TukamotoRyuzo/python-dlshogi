f10e50ebb77c8075079584877d9965a6ab41caeb
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2018-10-15 19:11:08 +0900

    update usi policy player to take modefile location as argment

--- pydlshogi/usi/usi_policy_player.py
+++ pydlshogi/usi/usi_policy_player.py
@@ -1,5 +1,11 @@
-﻿from pydlshogi.usi.usi import *
-from pydlshogi.player.policy_player import *
+﻿from pydlshogi.usi.usi import usi
+from pydlshogi.player.policy_player import PolicyPlayer
+import argparse
 
-player = PolicyPlayer()
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'modelfile', type=str, help='saved model weight file location')
+args = parser.parse_args()
+
+player = PolicyPlayer(args.modelfile)
 usi(player)
