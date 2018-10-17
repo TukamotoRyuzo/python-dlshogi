4bbaa59fdf5bc3ae371bac8ce4bf9cddb543c178
Naohiro Kobayashi <nkob@n-kobayashi-desktop>
2018-10-17 20:02:04 +0900

    change valuenet train script for Keras

--- train_value.py
+++ train_value.py
@@ -1,23 +1,20 @@
-﻿import numpy as np
-import chainer
-from chainer import cuda, Variable
-from chainer import optimizers, serializers
-import chainer.functions as F
-
-from pydlshogi.common import *
-from pydlshogi.network.value import ValueNetwork
-from pydlshogi.features import *
-from pydlshogi.read_kifu import *
-
-import argparse
+﻿import argparse
 import random
 import pickle
 import os
 import re
-
 import logging
 
+import numpy as np
+
+import pydlshogi.features as fts
+from pydlshogi.read_kifu import read_kifu
+from pydlshogi.network.value import ValueNetwork
+
+from keras.optimizers import SGD
+
 parser = argparse.ArgumentParser()
+# yapf: disable
 parser.add_argument('kifulist_train', type=str, help='train kifu list')
 parser.add_argument('kifulist_test', type=str, help='test kifu list')
 parser.add_argument('--batchsize', '-b', type=int, default=32, help='Number of positions in each mini-batch')
@@ -30,23 +27,23 @@ parser.add_argument('--resume', '-r', default='', help='Resume the optimization 
 parser.add_argument('--log', default=None, help='log file path')
 parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
 parser.add_argument('--eval_interval', '-i', type=int, default=1000, help='eval interval')
+# yapf: enable
 args = parser.parse_args()
 
-logging.basicConfig(format='%(asctime)s\t%(levelname)s\t%(message)s', datefmt='%Y/%m/%d %H:%M:%S', filename=args.log, level=logging.DEBUG)
+logging.basicConfig(
+    format='%(asctime)s\t%(levelname)s\t%(message)s',
+    datefmt='%Y/%m/%d %H:%M:%S',
+    filename=args.log,
+    level=logging.DEBUG)
 
-model = ValueNetwork()
-model.to_gpu()
-
-optimizer = optimizers.SGD(lr=args.lr)
-optimizer.setup(model)
+v_net = ValueNetwork()
+v_net.compile(SGD(lr=args.lr), 'categorical_crossentropy', metrics=['accuracy'])
+v_net.summary()
 
 # Init/Resume
 if args.initmodel:
     logging.info('Load model from {}'.format(args.initmodel))
-    serializers.load_npz(args.initmodel, model)
-if args.resume:
-    logging.info('Load optimizer state from {}'.format(args.resume))
-    serializers.load_npz(args.resume, optimizer)
+    v_net.load_weights(args.initmodel)
 
 logging.info('read kifu start')
 # 保存済みのpickleファイルがある場合、pickleファイルを読み込む
@@ -79,31 +76,34 @@ if not os.path.exists(test_pickle_filename):
     logging.info('save test pickle')
 logging.info('read kifu end')
 
-logging.info('train position num = {}'.format(len(positions_train)))
-logging.info('test position num = {}'.format(len(positions_test)))
+logging.info('train position num = %s', len(positions_train))
+logging.info('test position num = %s', len(positions_test))
+
 
 # mini batch
 def mini_batch(positions, i, batchsize):
     mini_batch_data = []
     mini_batch_win = []
     for b in range(batchsize):
-        features, move, win = make_features(positions[i + b])
+        features, move, win = fts.make_features(positions[i + b])
         mini_batch_data.append(features)
         mini_batch_win.append(win)
 
-    return (Variable(cuda.to_gpu(np.array(mini_batch_data, dtype=np.float32))),
-            Variable(cuda.to_gpu(np.array(mini_batch_win, dtype=np.int32).reshape((-1, 1)))))
+    return (np.array(mini_batch_data, dtype=np.float32),
+            np.array(mini_batch_win, dtype=np.int32).reshape((-1, 1)))
+
 
 def mini_batch_for_test(positions, batchsize):
     mini_batch_data = []
     mini_batch_win = []
     for b in range(batchsize):
-        features, move, win = make_features(random.choice(positions))
+        features, move, win = fts.make_features(random.choice(positions))
         mini_batch_data.append(features)
         mini_batch_win.append(win)
 
-    return (Variable(cuda.to_gpu(np.array(mini_batch_data, dtype=np.float32))),
-            Variable(cuda.to_gpu(np.array(mini_batch_win, dtype=np.int32).reshape((-1, 1)))))
+    return (np.array(mini_batch_data, dtype=np.float32),
+            np.array(mini_batch_win, dtype=np.int32).reshape((-1, 1)))
+
 
 # train
 logging.info('start training')
@@ -116,40 +116,37 @@ for e in range(args.epoch):
     sum_loss_epoch = 0
     for i in range(0, len(positions_train_shuffled) - args.batchsize, args.batchsize):
         x, t = mini_batch(positions_train_shuffled, i, args.batchsize)
-        y = model(x)
-
-        model.cleargrads()
-        loss = F.sigmoid_cross_entropy(y, t)
-        loss.backward()
-        optimizer.update()
-
+        hist = v_net.fit(x, t, batch_size=args.batch_size, epochs=1, verbose=0)
         itr += 1
-        sum_loss += loss.data
+        sum_loss += hist.history['loss'][0]
         itr_epoch += 1
-        sum_loss_epoch += loss.data
+        sum_loss_epoch += hist.history['loss'][0]
+        iteration = int(i / args.batchsize)
 
         # print train loss and test accuracy
-        if optimizer.t % args.eval_interval == 0:
+        if iteration % args.eval_interval == 0:
             x, t = mini_batch_for_test(positions_test, args.test_batchsize)
-            y = model(x)
-            logging.info('epoch = {}, iteration = {}, loss = {}, accuracy = {}'.format(optimizer.epoch + 1, optimizer.t, sum_loss / itr, F.binary_accuracy(y, t).data))
+            y = v_net.evaluate(x, t, verbose=0)
+            logging.info('epoch = %s, iteration = %s, loss = %s, accuracy = %s', e + 1,
+                         iteration, sum_loss / itr, y[1])
             itr = 0
             sum_loss = 0
 
     # validate test data
     logging.info('validate test data')
     itr_test = 0
+    sum_test_loss = 0
     sum_test_accuracy = 0
     for i in range(0, len(positions_test) - args.batchsize, args.batchsize):
         x, t = mini_batch(positions_test, i, args.batchsize)
-        y = model(x)
+        y = v_net.evaluate(x, t, verbose=0)
         itr_test += 1
-        sum_test_accuracy += F.binary_accuracy(y, t).data
-    logging.info('epoch = {}, iteration = {}, train loss avr = {}, test accuracy = {}'.format(optimizer.epoch + 1, optimizer.t, sum_loss_epoch / itr_epoch, sum_test_accuracy / itr_test))
-    
-    optimizer.new_epoch()
+        sum_test_loss += y[0]
+        sum_test_accuracy += y[1]
+    logging.info('epoch = %s, iteration = %s, test loss = %s, test accuracy = %s',
+                 e + 1, iteration, sum_test_loss / itr_test,
+                 sum_test_accuracy / itr_test)
+
 
 logging.info('save the model')
-serializers.save_npz(args.model, model)
-logging.info('save the optimizer')
-serializers.save_npz(args.state, optimizer)
+v_net.save_weights(args.model)
