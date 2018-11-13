4caa7b81d7e7ab405aa6832a170ee2b9f606cb1a
Naohiro Kobayashi <nkob@n-kobayashi-desktop>
2018-11-13 12:48:51 +0900

    add Keras version for PolicyValueNetwork

--- pydlshogi/network/policy_value.py
+++ pydlshogi/network/policy_value.py
@@ -1,53 +1,41 @@
-﻿from chainer import Chain
-import chainer.functions as F
-import chainer.links as L
+﻿from keras.layers import Activation, Input, Dense, Conv2D, Reshape
+from keras.models import Model
 
-from pydlshogi.common import *
+from pydlshogi.common import MOVE_DIRECTION_LABEL_NUM
 
 ch = 192
 fcl = 256
-class PolicyValueNetwork(Chain):
-    def __init__(self):
-        super(PolicyValueNetwork, self).__init__()
-        with self.init_scope():
-            self.l1=L.Convolution2D(in_channels = 104, out_channels = ch, ksize = 3, pad = 1)
-            self.l2=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l3=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l4=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l5=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l6=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l7=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l8=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l9=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l10=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l11=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            self.l12=L.Convolution2D(in_channels = ch, out_channels = ch, ksize = 3, pad = 1)
-            # policy network
-            self.l13=L.Convolution2D(in_channels = ch, out_channels = MOVE_DIRECTION_LABEL_NUM, ksize = 1, nobias = True)
-            self.l13_bias=L.Bias(shape=(9*9*MOVE_DIRECTION_LABEL_NUM))
-            # value network
-            self.l13_v=L.Convolution2D(in_channels = ch, out_channels = MOVE_DIRECTION_LABEL_NUM, ksize = 1)
-            self.l14_v=L.Linear(9*9*MOVE_DIRECTION_LABEL_NUM, fcl)
-            self.l15_v=L.Linear(fcl, 1)
 
-    def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        h2 = F.relu(self.l2(h1))
-        h3 = F.relu(self.l3(h2))
-        h4 = F.relu(self.l4(h3))
-        h5 = F.relu(self.l5(h4))
-        h6 = F.relu(self.l6(h5))
-        h7 = F.relu(self.l7(h6))
-        h8 = F.relu(self.l8(h7))
-        h9 = F.relu(self.l9(h8))
-        h10 = F.relu(self.l10(h9))
-        h11 = F.relu(self.l11(h10))
-        h12 = F.relu(self.l12(h11))
+
+class PolicyValueNetwork(Model):
+
+    def __init__(self):
+        # yapf: disable
+        inputs = Input((104, 9, 9))
+        h1 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(inputs)
+        h2 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h1)
+        h3 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h2)
+        h4 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h3)
+        h5 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h4)
+        h6 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h5)
+        h7 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h6)
+        h8 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h7)
+        h9 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h8)
+        h10 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h9)
+        h11 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h10)
+        h12 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h11)
+
         # policy network
-        h13 = self.l13(h12)
-        policy = self.l13_bias(F.reshape(h13, (-1, 9*9*MOVE_DIRECTION_LABEL_NUM)))
+        h13_p = Conv2D(MOVE_DIRECTION_LABEL_NUM, 1, activation='relu', padding='same', data_format="channels_first")(h12)
+        h13_p = Reshape((MOVE_DIRECTION_LABEL_NUM * 9 * 9,))(h13_p)
+        out_policy = Activation('softmax', name='policy_output')(h13_p)
+
         # value network
-        h13_v = F.relu(self.l13_v(h12))
-        h14_v = F.relu(self.l14_v(h13_v))
-        value = self.l15_v(h14_v)
-        return policy, value
+        h13_v = Conv2D(MOVE_DIRECTION_LABEL_NUM, 1, activation='relu', padding='same', data_format="channels_first")(h12)
+        h13_v = Reshape((MOVE_DIRECTION_LABEL_NUM * 9 * 9,))(h13_v)
+        h14_v = Dense(fcl, activation='relu')(h13_v)
+        h15_v = Dense(1)(h14_v)
+        out_value = Activation('sigmoid', name='value_output')(h15_v)
+        # yapf: enable
+
+        super().__init__(inputs=inputs, outputs=[out_policy, out_value])
--- train_policy_value.py
+++ train_policy_value.py
@@ -1,23 +1,22 @@
-﻿import numpy as np
-import chainer
-from chainer import cuda, Variable
-from chainer import optimizers, serializers
-import chainer.functions as F
-
-from pydlshogi.common import *
-from pydlshogi.network.policy_value import PolicyValueNetwork
-from pydlshogi.features import *
-from pydlshogi.read_kifu import *
-
-import argparse
-import random
-import pickle
+﻿import argparse
+import logging
 import os
+import pickle
+import random
 import re
 
-import logging
+from keras.metrics import binary_accuracy, categorical_accuracy
+from keras.optimizers import SGD
+
+import numpy as np
+
+import pydlshogi.features as fts
+from pydlshogi.network.policy_value import PolicyValueNetwork
+from pydlshogi.read_kifu import read_kifu
+
 
 parser = argparse.ArgumentParser()
+# yapf: disable
 parser.add_argument('kifulist_train', type=str, help='train kifu list')
 parser.add_argument('kifulist_test', type=str, help='test kifu list')
 parser.add_argument('--batchsize', '-b', type=int, default=32, help='Number of positions in each mini-batch')
@@ -30,23 +29,32 @@ parser.add_argument('--resume', '-r', default='', help='Resume the optimization 
 parser.add_argument('--log', default=None, help='log file path')
 parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
 parser.add_argument('--eval_interval', '-i', type=int, default=1000, help='eval interval')
+# yapf: enable
 args = parser.parse_args()
 
-logging.basicConfig(format='%(asctime)s\t%(levelname)s\t%(message)s', datefmt='%Y/%m/%d %H:%M:%S', filename=args.log, level=logging.DEBUG)
+logging.basicConfig(
+    format='%(asctime)s\t%(levelname)s\t%(message)s',
+    datefmt='%Y/%m/%d %H:%M:%S',
+    filename=args.log,
+    level=logging.DEBUG)
 
-model = PolicyValueNetwork()
-model.to_gpu()
-
-optimizer = optimizers.SGD(lr=args.lr)
-optimizer.setup(model)
+pv_net = PolicyValueNetwork()
+pv_net.compile(
+    optimizer=SGD(lr=args.lr),
+    loss={
+        'policy_output': 'categorical_crossentropy',
+        'value_output': 'binary_crossentropy'
+    },
+    loss_weights={
+        'policy_output': 1,
+        'value_output': 1
+    })
+pv_net.summary()
 
 # Init/Resume
 if args.initmodel:
-    logging.info('Load model from {}'.format(args.initmodel))
-    serializers.load_npz(args.initmodel, model)
-if args.resume:
-    logging.info('Load optimizer state from {}'.format(args.resume))
-    serializers.load_npz(args.resume, optimizer)
+    logging.info('Load model from %s', args.initmodel)
+    pv_net.load_weights(args.initmodel)
 
 logging.info('read kifu start')
 # 保存済みのpickleファイルがある場合、pickleファイルを読み込む
@@ -79,8 +87,9 @@ if not os.path.exists(test_pickle_filename):
     logging.info('save test pickle')
 logging.info('read kifu end')
 
-logging.info('train position num = {}'.format(len(positions_train)))
-logging.info('test position num = {}'.format(len(positions_test)))
+logging.info('train position num = %s', len(positions_train))
+logging.info('test position num = %s', len(positions_test))
+
 
 # mini batch
 def mini_batch(positions, i, batchsize):
@@ -88,33 +97,38 @@ def mini_batch(positions, i, batchsize):
     mini_batch_move = []
     mini_batch_win = []
     for b in range(batchsize):
-        features, move, win = make_features(positions[i + b])
+        features, move, win = fts.make_features(positions[i + b])
         mini_batch_data.append(features)
         mini_batch_move.append(move)
         mini_batch_win.append(win)
 
-    return (Variable(cuda.to_gpu(np.array(mini_batch_data, dtype=np.float32))),
-            Variable(cuda.to_gpu(np.array(mini_batch_move, dtype=np.int32))),
-            Variable(cuda.to_gpu(np.array(mini_batch_win, dtype=np.int32).reshape((-1, 1)))))
+    return (np.array(mini_batch_data, dtype=np.float32),
+            np.array(mini_batch_move, dtype=np.int32),
+            np.array(mini_batch_win, dtype=np.int32).reshape((-1, 1)))
+
 
 def mini_batch_for_test(positions, batchsize):
     mini_batch_data = []
     mini_batch_move = []
     mini_batch_win = []
     for b in range(batchsize):
-        features, move, win = make_features(random.choice(positions))
+        features, move, win = fts.make_features(random.choice(positions))
         mini_batch_data.append(features)
         mini_batch_move.append(move)
         mini_batch_win.append(win)
 
-    return (Variable(cuda.to_gpu(np.array(mini_batch_data, dtype=np.float32))),
-            Variable(cuda.to_gpu(np.array(mini_batch_move, dtype=np.int32))),
-            Variable(cuda.to_gpu(np.array(mini_batch_win, dtype=np.int32).reshape((-1, 1)))))
+    return (np.array(mini_batch_data, dtype=np.float32),
+            np.array(mini_batch_move, dtype=np.int32),
+            np.array(mini_batch_win, dtype=np.int32).reshape((-1, 1)))
+
 
 # train
 logging.info('start training')
 itr = 0
 sum_loss = 0
+loss_hist = []
+acc_p_hist = []
+acc_v_hist = []
 for e in range(args.epoch):
     positions_train_shuffled = random.sample(positions_train, len(positions_train))
 
@@ -122,27 +136,39 @@ for e in range(args.epoch):
     sum_loss_epoch = 0
     for i in range(0, len(positions_train_shuffled) - args.batchsize, args.batchsize):
         x, t1, t2 = mini_batch(positions_train_shuffled, i, args.batchsize)
-        y1, y2 = model(x)
-
-        model.cleargrads()
-        loss = F.softmax_cross_entropy(y1, t1) + F.sigmoid_cross_entropy(y2, t2)
-        loss.backward()
-        optimizer.update()
+        hist = pv_net.fit(
+            x, {
+                'policy_output': t1,
+                'value_output': t2
+            },
+            batch_size=args.batch_size,
+            epochs=1,
+            verbose=0)
 
         itr += 1
-        sum_loss += loss.data
+        sum_loss += hist.history['loss'][0]
         itr_epoch += 1
-        sum_loss_epoch += loss.data
+        sum_loss_epoch += hist.history['loss'][0]
+        iteration = int(i / args.batchsize)
 
         # print train loss and test accuracy
-        if optimizer.t % args.eval_interval == 0:
+        if iteration % args.eval_interval == 0:
             x, t1, t2 = mini_batch_for_test(positions_test, args.test_batchsize)
-            y1, y2 = model(x)
-            logging.info('epoch = {}, iteration = {}, loss = {}, accuracy = {}, {}'.format(
-                optimizer.epoch + 1, optimizer.t, sum_loss / itr,
-                F.accuracy(y1, t1).data, F.binary_accuracy(y2, t2).data))
+            y1, y2 = pv_net.evaluate(
+                x, {
+                    'policy_output': t1,
+                    'value_output': t2
+                }, verbose=0)
+            logging.info('epoch = %s, iteration = %s, loss = %s, accuracy = %s, %s',
+                         e + 1, iteration, sum_loss / itr,
+                         categorical_accuracy(y1, t1),
+                         binary_accuracy(y2, t2))
             itr = 0
             sum_loss = 0
+
+    loss_hist.append(hist.history['loss'][0])
+    acc_p_hist.append(hist.history['acc'][0])
+    acc_v_hist.append(hist.history['acc'][1])
 
     # validate test data
     logging.info('validate test data')
@@ -151,17 +177,21 @@ for e in range(args.epoch):
     sum_test_accuracy2 = 0
     for i in range(0, len(positions_test) - args.batchsize, args.batchsize):
         x, t1, t2 = mini_batch(positions_test, i, args.batchsize)
-        y1, y2 = model(x)
+        y1, y2 = pv_net.evaluate(
+            x, {
+                'policy_output': t1,
+                'value_output': t2
+            }, verbose=0)
         itr_test += 1
-        sum_test_accuracy1 += F.accuracy(y1, t1).data
-        sum_test_accuracy2 += F.binary_accuracy(y2, t2).data
-    logging.info('epoch = {}, iteration = {}, train loss avr = {}, test accuracy = {}, {}'.format(
-        optimizer.epoch + 1, optimizer.t, sum_loss_epoch / itr_epoch,
-        sum_test_accuracy1 / itr_test, sum_test_accuracy2 / itr_test))
-    
-    optimizer.new_epoch()
+        sum_test_accuracy1 += categorical_accuracy(y1, t1)
+        sum_test_accuracy2 += binary_accuracy(y2, t2)
+    logging.info(
+        'epoch = %s, iteration = %s, train loss avr = %s, test accuracy = %s, %s',
+        e + 1, iteration, sum_loss_epoch / itr_epoch, sum_test_accuracy1 / itr_test,
+        sum_test_accuracy2 / itr_test)
 
 logging.info('save the model')
-serializers.save_npz(args.model, model)
-logging.info('save the optimizer')
-serializers.save_npz(args.state, optimizer)
+if args.initmodel:
+    pv_net.save_weights(args.initmodel)
+else:
+    pv_net.save_weights('init.h5')
