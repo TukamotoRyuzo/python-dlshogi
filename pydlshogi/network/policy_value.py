c4779a63156563454ce2917794a7dcad570c1be0
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2018-11-22 18:42:57 +0900

    refactor policyvalue network

--- pydlshogi/network/policy_value.py
+++ pydlshogi/network/policy_value.py
@@ -1,4 +1,4 @@
-﻿from keras.layers import Activation, Input, Dense, Conv2D, Reshape
+﻿from keras import layers
 from keras.models import Model
 
 from pydlshogi.common import MOVE_DIRECTION_LABEL_NUM
@@ -6,36 +6,67 @@ from pydlshogi.common import MOVE_DIRECTION_LABEL_NUM
 ch = 192
 fcl = 256
 
+num_classes = MOVE_DIRECTION_LABEL_NUM * 9 * 9
 
-class PolicyValueNetwork(Model):
 
-    def __init__(self):
-        # yapf: disable
-        inputs = Input((104, 9, 9))
-        h1 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(inputs)
-        h2 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h1)
-        h3 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h2)
-        h4 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h3)
-        h5 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h4)
-        h6 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h5)
-        h7 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h6)
-        h8 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h7)
-        h9 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h8)
-        h10 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h9)
-        h11 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h10)
-        h12 = Conv2D(ch, 3, activation='relu', padding='same', data_format="channels_first")(h11)
+def PolicyValueNetwork():
+    # input
+    board_image = layers.Input(shape=(104, 9, 9))
 
-        # policy network
-        h13_p = Conv2D(MOVE_DIRECTION_LABEL_NUM, 1, activation='relu', padding='same', data_format="channels_first")(h12)
-        h13_p = Reshape((MOVE_DIRECTION_LABEL_NUM * 9 * 9,))(h13_p)
-        out_policy = Activation('softmax', name='policy_output')(h13_p)
+    # common part
+    x = _conv_block(board_image, 1)
+    x = _conv_block(x, 2)
+    x = _conv_block(x, 3)
+    x = _conv_block(x, 4)
+    x = _conv_block(x, 5)
+    x = _conv_block(x, 6)
+    x = _conv_block(x, 7)
+    x = _conv_block(x, 8)
+    x = _conv_block(x, 9)
+    x = _conv_block(x, 10)
+    x = _conv_block(x, 11)
+    x = _conv_block(x, 12)
+    common_out = _conv_block(x, 13)
 
-        # value network
-        h13_v = Conv2D(MOVE_DIRECTION_LABEL_NUM, 1, activation='relu', padding='same', data_format="channels_first")(h12)
-        h13_v = Reshape((MOVE_DIRECTION_LABEL_NUM * 9 * 9,))(h13_v)
-        h14_v = Dense(fcl, activation='relu')(h13_v)
-        h15_v = Dense(1)(h14_v)
-        out_value = Activation('sigmoid', name='value_output')(h15_v)
-        # yapf: enable
+    # policy part
+    x = layers.Conv2D(
+        MOVE_DIRECTION_LABEL_NUM,
+        1,
+        padding='same',
+        data_format="channels_first",
+        name='policy_conv_out')(common_out)
+    x = layers.Reshape((num_classes,), name='policy_reshape')(x)
+    policy_out = layers.Activation('softmax', name='policy_out')(x)
 
-        super().__init__(inputs=inputs, outputs=[out_policy, out_value])
+    # value part
+    x = layers.Conv2D(
+        MOVE_DIRECTION_LABEL_NUM,
+        1,
+        padding='same',
+        activation='relu',
+        kernel_initializer='he_normal',
+        data_format="channels_first",
+        name='value_conv_out')(common_out)
+    x = layers.Reshape((num_classes,), name='value_reshape')(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.Dense(fcl, activation='relu', name='value_dense_1')(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.Dense(1, name='value_dense_2')(x)
+    value_out = layers.Activation('sigmoid', name='value_out')(x)
+
+    policy_value_model = Model(inputs=board_image, outputs=[policy_out, value_out])
+
+    return policy_value_model
+
+
+def _conv_block(x, block_id):
+    x = layers.Conv2D(
+        ch,
+        3,
+        padding='same',
+        data_format="channels_first",
+        kernel_initializer='he_normal',
+        name='conv_{}'.format(block_id))(x)
+    x = layers.BatchNormalization(axis=1, name='conv_{}_bn'.format(block_id))(x)
+    x = layers.ReLU(name='conv_{}_relu'.format(block_id))(x)
+    return x
