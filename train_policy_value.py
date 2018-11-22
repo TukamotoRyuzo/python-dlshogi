7d01e0a88f340a25770c349cbaedee9e6ecb4c69
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2018-11-22 18:43:53 +0900

    refactor policyvalue training script

--- train_policy_value.py
+++ train_policy_value.py
@@ -1,21 +1,16 @@
 ﻿import argparse
 import logging
-import os
-import pickle
 import random
-import re
 
 from keras.metrics import binary_accuracy, categorical_accuracy
 from keras.optimizers import SGD
 
+import pydlshogi.common as cmn
+from pydlshogi.network.policy_value import PolicyValueNetwork
+
 from tqdm import tqdm
 
-import numpy as np
-
-import pydlshogi.common as cmn
-import pydlshogi.features as fts
-from pydlshogi.network.policy_value import PolicyValueNetwork
-from pydlshogi.read_kifu import read_kifu
+from train_tools import Status, create_mini_batch, load_kifu_data
 
 parser = argparse.ArgumentParser()
 # yapf: disable
@@ -42,16 +37,19 @@ logging.basicConfig(
 
 pv_net = PolicyValueNetwork()
 pv_net.compile(
-    optimizer=SGD(lr=args.lr),
+    optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True),
     loss={
-        'policy_output': 'categorical_crossentropy',
-        'value_output': 'binary_crossentropy'
+        'policy_out': 'categorical_crossentropy',
+        'value_out': 'binary_crossentropy'
+    },
+    metrics={
+        'policy_out': categorical_accuracy,
+        'value_out': binary_accuracy
     },
     loss_weights={
-        'policy_output': 1,
-        'value_output': 1
-    },
-    metrics=['accuracy'])
+        'policy_out': 0.5,
+        'value_out': 1.5
+    })
 pv_net.summary()
 
 # Init/Resume
@@ -59,142 +57,69 @@ if args.initmodel:
     logging.info('Load model from %s', args.initmodel)
     pv_net.load_weights(args.initmodel)
 
-logging.info('read kifu start')
-# 保存済みのpickleファイルがある場合、pickleファイルを読み込む
-# train date
-train_pickle_filename = re.sub(r'\..*?$', '', args.kifulist_train) + '.pickle'
-if os.path.exists(train_pickle_filename):
-    with open(train_pickle_filename, 'rb') as f:
-        positions_train = pickle.load(f)
-    logging.info('load train pickle')
-else:
-    positions_train = read_kifu(args.kifulist_train)
+positions_train, positions_test = load_kifu_data(args)
 
-# test data
-test_pickle_filename = re.sub(r'\..*?$', '', args.kifulist_test) + '.pickle'
-if os.path.exists(test_pickle_filename):
-    with open(test_pickle_filename, 'rb') as f:
-        positions_test = pickle.load(f)
-    logging.info('load test pickle')
-else:
-    positions_test = read_kifu(args.kifulist_test)
+num_classes = 9 * 9 * cmn.MOVE_DIRECTION_LABEL_NUM
 
-# 保存済みのpickleがない場合、pickleファイルを保存する
-if not os.path.exists(train_pickle_filename):
-    with open(train_pickle_filename, 'wb') as f:
-        pickle.dump(positions_train, f, pickle.HIGHEST_PROTOCOL)
-    logging.info('save train pickle')
-if not os.path.exists(test_pickle_filename):
-    with open(test_pickle_filename, 'wb') as f:
-        pickle.dump(positions_test, f, pickle.HIGHEST_PROTOCOL)
-    logging.info('save test pickle')
-logging.info('read kifu end')
-
-logging.info('train position num = %s', len(positions_train))
-logging.info('test position num = %s', len(positions_test))
-
-
-# mini batch
-def mini_batch(positions, i, batchsize):
-    mini_batch_data = []
-    mini_batch_move = []
-    mini_batch_win = []
-    for b in range(batchsize):
-        features, move, win = fts.make_features(positions[i + b])
-        mini_batch_data.append(features)
-        mini_batch_move.append(move)
-        mini_batch_win.append(win)
-    mini_batch_move = np.identity(
-        9 * 9 * cmn.MOVE_DIRECTION_LABEL_NUM)[mini_batch_move]
-
-    return (np.array(mini_batch_data, dtype=np.float32),
-            np.array(mini_batch_move, dtype=np.int32),
-            np.array(mini_batch_win, dtype=np.int32).reshape((-1, 1)))
-
-
-def mini_batch_for_test(positions, batchsize):
-    mini_batch_data = []
-    mini_batch_move = []
-    mini_batch_win = []
-    for b in range(batchsize):
-        features, move, win = fts.make_features(random.choice(positions))
-        mini_batch_data.append(features)
-        mini_batch_move.append(move)
-        mini_batch_win.append(win)
-
-    mini_batch_move = np.identity(
-        9 * 9 * cmn.MOVE_DIRECTION_LABEL_NUM)[mini_batch_move]
-    return (np.array(mini_batch_data, dtype=np.float32),
-            np.array(mini_batch_move, dtype=np.int32),
-            np.array(mini_batch_win, dtype=np.int32).reshape((-1, 1)))
-
-
-# train
+# train & validation
 logging.info('start training')
-itr = 0
-sum_loss = 0
-sum_acc_p = 0
-sum_acc_v = 0
+train_size = len(positions_train)
 for e in range(args.epoch):
-    positions_train_shuffled = random.sample(positions_train,
-                                             len(positions_train))
+    # train
+    train_status = Status()
 
-    for i in tqdm(
-            range(0,
-                  len(positions_train_shuffled) - args.batchsize,
-                  args.batchsize)):
-        x, t1, t2 = mini_batch(positions_train_shuffled, i, args.batchsize)
-        hist = pv_net.fit(
-            x, {
-                'policy_output': t1,
-                'value_output': t2
-            },
-            batch_size=args.batchsize,
-            epochs=1,
-            verbose=0)
+    positions_train_shuffled = random.sample(positions_train, train_size)
 
-        itr += 1
-        sum_loss += hist.history['loss'][0]
-        sum_acc_p += hist.history['policy_output_acc'][0]
-        sum_acc_v += hist.history['value_output_acc'][0]
-
-        iteration = int(i / args.batchsize)
+    interval_status = Status()
+    for batch_pos in tqdm(range(0, train_size - args.batchsize, args.batchsize)):
+        x, t1, t2 = create_mini_batch(positions_train_shuffled, batch_pos,
+                                      args.batchsize)
+        results = pv_net.train_on_batch(x, {'policy_out': t1, 'value_out': t2})
+        train_status.update(results)
+        interval_status.update(results)
 
         # print train loss and accuracy
-        if iteration % args.eval_interval == 0:
+        if train_status.iter_num % args.eval_interval == 0:
+            interval_status.calc_mean()
             logging.info(
-                'epoch = %s, iteration = %s, train loss = %s, train accuracy = %s, %s',
-                e + 1, iteration, sum_loss / itr, sum_acc_p / itr, sum_acc_v / itr)
-            itr = 0
-            sum_loss = 0
-            sum_acc_p = 0
-            sum_acc_v = 0
+                'epoch = %s, iteration = %s, interval loss = %s(p: %s, v: %s), interval accuracy = p: %s, v: %s',
+                e + 1,
+                train_status.iter_num,
+                "{:.4f}".format(interval_status.total_loss),
+                "{:.4f}".format(interval_status.policy_loss),
+                "{:.4f}".format(interval_status.value_loss),
+                "{:.4f}".format(interval_status.policy_acc),
+                "{:.4f}".format(interval_status.value_acc))  # yapf: disable
+            interval_status.reset()
+
+    end_pos = batch_pos + args.batchsize
+    remain_size = train_size - end_pos
+    if remain_size > 0:
+        x, t1, t2 = create_mini_batch(positions_train_shuffled, end_pos, remain_size)
+        results = pv_net.train_on_batch(x, {'policy_out': t1, 'value_out': t2})
+        train_status.update(results)
 
     # validate test data
     logging.info('validate test data')
-    itr_test = 0
-    sum_train_loss = 0
-    sum_test_accuracy1 = 0
-    sum_test_accuracy2 = 0
+    test_status = Status()
     for i in range(0, len(positions_test) - args.batchsize, args.batchsize):
-        x, t1, t2 = mini_batch(positions_test, i, args.batchsize)
-        [loss, policy_loss, value_loss, policy_acc,
-         value_acc] = pv_net.evaluate(
-             x, {
-                 'policy_output': t1,
-                 'value_output': t2
-             }, verbose=0)
-        itr_test += 1
-        sum_train_loss += loss
-        sum_test_accuracy1 += policy_acc
-        sum_test_accuracy2 += value_acc
-    logging.info(
-        'epoch = %s, iteration = %s, test loss = %s, test accuracy = %s, %s',
-        e + 1, iteration, sum_train_loss / itr_test,
-        sum_test_accuracy1 / itr_test, sum_test_accuracy2 / itr_test)
+        x, t1, t2 = create_mini_batch(positions_test, i, args.batchsize)
+        result = pv_net.test_on_batch(x, {'policy_out': t1, 'value_out': t2})
+        test_status.update(result)
 
+    test_status.calc_mean()
+    logging.info(
+        'validation finished: epoch = %s, iteration = %s, test loss = %s(p: %s, v: %s), test accuracy = p: %s, v: %s',
+        e + 1,
+        test_status.iter_num,
+        "{:.4f}".format(test_status.total_loss),
+        "{:.4f}".format(test_status.policy_loss),
+        "{:.4f}".format(test_status.value_loss),
+        "{:.4f}".format(test_status.policy_acc),
+        "{:.4f}".format(test_status.value_acc))  # yapf: disable
+
+    logging.info("epoch %s finished", e + 1)
     logging.info('save the model')
-    if args.initmodel:
-        pv_net.save_weights(args.initmodel)
-    else:
-        pv_net.save_weights('init_valuepolicy_epoch{}.h5'.format(e + 1))
+    pv_net.save_weights('init_valuepolicy_epoch{}.h5'.format(e + 1))
+
+    pv_net.optimizer.lr = pv_net.optimizer.lr * 0.92
---
+++ train_tools.py
@@ -0,0 +1,108 @@
+import logging
+import os
+import pickle
+import re
+
+from keras.utils import to_categorical
+
+import numpy as np
+
+import pydlshogi.common as cmn
+import pydlshogi.features as fts
+from pydlshogi.read_kifu import read_kifu
+
+num_classes = 9 * 9 * cmn.MOVE_DIRECTION_LABEL_NUM
+
+
+def create_mini_batch(positions, batch_head, batch_size):
+    mini_batch_data = np.empty((batch_size, 104, 9, 9), dtype=np.float)
+    mini_batch_move = np.empty((batch_size), dtype=np.int)
+    mini_batch_win = np.empty((batch_size), dtype=np.int)
+    for b in range(batch_size):
+        features, move, win = fts.make_features(positions[batch_head + b])
+        mini_batch_data[b] = features
+        mini_batch_move[b] = move
+        mini_batch_win[b] = win
+    mini_batch_move = to_categorical(mini_batch_move, num_classes=num_classes)
+
+    return (mini_batch_data, mini_batch_move, mini_batch_win.reshape((-1, 1)))
+
+
+def load_kifu_data(args):
+    logging.info('read kifu start')
+    # 保存済みのpickleファイルがある場合、pickleファイルを読み込む
+    # train data
+    train_pickle_filename = re.sub(r'\..*?$', '', args.kifulist_train) + '.pickle'
+    if os.path.exists(train_pickle_filename):
+        with open(train_pickle_filename, 'rb') as f:
+            positions_train = pickle.load(f)
+            logging.info('load train pickle')
+    else:
+        positions_train = read_kifu(args.kifulist_train)
+
+    # test data
+    test_pickle_filename = re.sub(r'\..*?$', '', args.kifulist_test) + '.pickle'
+    if os.path.exists(test_pickle_filename):
+        with open(test_pickle_filename, 'rb') as f:
+            positions_test = pickle.load(f)
+            logging.info('load test pickle')
+    else:
+        positions_test = read_kifu(args.kifulist_test)
+
+    # 保存済みのpickleがない場合、pickleファイルを保存する
+    if not os.path.exists(train_pickle_filename):
+        with open(train_pickle_filename, 'wb') as f:
+            pickle.dump(positions_train, f, pickle.HIGHEST_PROTOCOL)
+            logging.info('save train pickle')
+    if not os.path.exists(test_pickle_filename):
+        with open(test_pickle_filename, 'wb') as f:
+            pickle.dump(positions_test, f, pickle.HIGHEST_PROTOCOL)
+            logging.info('save test pickle')
+    logging.info('read kifu end')
+
+    logging.info('train position num = %s', len(positions_train))
+    logging.info('test position num = %s', len(positions_test))
+
+    return positions_train, positions_test
+
+
+class Status:
+
+    def __init__(self):
+        self.policy_loss_sum = 0
+        self.policy_acc_sum = 0
+        self.value_loss_sum = 0
+        self.value_acc_sum = 0
+        self.total_loss_sum = 0
+
+        self.iter_num = 0
+
+        self.policy_loss = 0
+        self.policy_acc = 0
+        self.value_loss = 0
+        self.value_acc = 0
+        self.total_loss = 0
+
+    def update(self, results):
+        self.total_loss_sum += results[0]
+        self.policy_loss_sum += results[1]
+        self.value_loss_sum += results[2]
+        self.policy_acc_sum += results[3]
+        self.value_acc_sum += results[4]
+        self.iter_num += 1
+
+    def calc_mean(self):
+        self.policy_loss = self.policy_loss_sum / self.iter_num
+        self.policy_acc = self.policy_acc_sum / self.iter_num
+        self.value_loss = self.value_loss_sum / self.iter_num
+        self.value_acc = self.value_acc_sum / self.iter_num
+        self.total_loss = self.total_loss_sum / self.iter_num
+
+    def reset(self):
+        self.policy_loss_sum = 0
+        self.policy_acc_sum = 0
+        self.value_loss_sum = 0
+        self.value_acc_sum = 0
+        self.total_loss_sum = 0
+
+        self.iter_num = 0
