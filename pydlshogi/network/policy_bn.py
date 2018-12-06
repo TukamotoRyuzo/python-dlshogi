6a5f94f01269c95f5e1ddd7c38583558cb8db041
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2018-12-06 11:42:03 +0900

    add Chainer version of PolicyNetworkBN

--- pydlshogi/network/policy_bn.py
+++ pydlshogi/network/policy_bn.py
@@ -1,52 +1,38 @@
-﻿from keras import layers
-from keras.models import Model
-
-from pydlshogi.common import MOVE_DIRECTION_LABEL_NUM
+﻿import chainer
+import chainer.links as L
+import chainer.functions as F
 
 _ch = 192
-_num_classes = MOVE_DIRECTION_LABEL_NUM * 9 * 9
 
 
-def PolicyNetwork():
-    # input
-    board_image = layers.Input(shape=(9, 9, 104))
+class PolicyNetwork(chainer.ChainList):
 
-    # convolution
-    x = _conv_block(board_image, 1)
-    x = _conv_block(x, 2)
-    x = _conv_block(x, 3)
-    x = _conv_block(x, 4)
-    x = _conv_block(x, 5)
-    x = _conv_block(x, 6)
-    x = _conv_block(x, 7)
-    x = _conv_block(x, 8)
-    x = _conv_block(x, 9)
-    x = _conv_block(x, 10)
-    x = _conv_block(x, 11)
-    x = _conv_block(x, 12)
-    x = _conv_block(x, 13)
+    def __init__(self):
+        super().__init__(
+            _PNBlock(104), _PNBlock(_ch), _PNBlock(_ch), _PNBlock(_ch), _PNBlock(_ch),
+            _PNBlock(_ch), _PNBlock(_ch), _PNBlock(_ch), _PNBlock(_ch), _PNBlock(_ch),
+            _PNBlock(_ch), _PNBlock(_ch), _PNBlock(_ch))
 
-    # output
-    x = layers.Conv2D(
-        MOVE_DIRECTION_LABEL_NUM,
-        1,
-        padding='same',
-        name='conv_out')(x)
-    x = layers.Reshape((_num_classes,), name='reshape')(x)
-    movement_probability = layers.Activation('softmax', name='output')(x)
-
-    policy_model = Model(inputs=board_image, outputs=movement_probability)
-
-    return policy_model
+    def forward(self, x):
+        for f in self.children():
+            x = f(x)
+        if chainer.config.train:
+            return x
+        return F.softmax(x)
 
 
-def _conv_block(x, block_id):
-    x = layers.Conv2D(
-        _ch,
-        3,
-        padding='same',
-        kernel_initializer='he_normal',
-        name='conv_{}'.format(block_id))(x)
-    x = layers.BatchNormalization(name='conv_{}_bn'.format(block_id))(x)
-    x = layers.ReLU(name='conv_{}_relu'.format(block_id))(x)
-    return x
+class _PNBlock(chainer.Chain):
+
+    def __init__(self, in_channels):
+        super().__init__()
+        he_w = chainer.initializers.HeNormal()
+        with self.init_scope():
+            self.conv = L.Convolution2D(
+                in_channels, _ch, ksize=3, pad=1, nobias=True, initialW=he_w)
+            self.bn = L.BatchNormalization(_ch)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = F.relu(x)
+        return x
